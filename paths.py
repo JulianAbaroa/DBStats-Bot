4182cf93bc414e8f5969d87ed91c0d9434e425fd
placeholder <julianabaroa38@gmail.com>
2025-08-26 13:20:58 -0400

    fix (paths): added dbstats exe path

--- paths.py
+++ paths.py
@@ -2,7 +2,9 @@ import os
 
 media_path = "/media/maste"
 drives = [os.path.join(media_path, d) for d in os.listdir(media_path)]
+script_dir = os.path.dirname(os.path.abspath(__file__))
 halo_drive = None
+
 
 for d in drives:
     if os.path.isdir(os.path.join(d, "Halo")):
@@ -10,12 +12,12 @@ for d in drives:
         break
 
 if not halo_drive:
-    raise RuntimeError("No se encontró el disco duro externo con Halo montado")
+    raise RuntimeError("External hard drive with Halo mounted was not found")
 
-# Rutas
 CARNAGES_DIRECTORY = os.path.join(halo_drive, "Halo", "Carnages")
 DATABASE_PATH = os.path.join(halo_drive, "Halo", "DBStats DataBase", "dbstats.db")
 PROCESSED_DIRECTORY = os.path.join(CARNAGES_DIRECTORY, "Processed")
+DBSTATS_EXE_PATH = os.path.normpath(os.path.join(script_dir, "..", "DBStats", "publish", "DBStats.dll"))
 
 print(f"Disco externo detectado en: {halo_drive}")
 print(f"Carnages: {CARNAGES_DIRECTORY}")
