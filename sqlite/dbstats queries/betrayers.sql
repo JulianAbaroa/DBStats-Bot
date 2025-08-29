8a6617362ddc8d40358c9f745faed75238ab44b6
placeholder <julianabaroa38@gmail.com>
2025-08-29 13:35:36 -0400

    fix (query): betrayers query modified

--- sqlite/dbstats queries/betrayers.sql
+++ sqlite/dbstats queries/betrayers.sql
@@ -1,10 +1,10 @@
 SELECT
     P.player_name,
     P.player_id,
-    SUM(1) AS total_betrays
+    SUM(Pen.betrayals) AS total_betrays
 FROM Penalties AS Pen
 JOIN Players AS PL ON Pen.player_match_id = PL.player_match_id
 JOIN Profiles AS P ON PL.player_id = P.player_id
-WHERE Pen.penalty_type = 'betrays'
 GROUP BY P.player_id, P.player_name
+HAVING total_betrays > 0
 ORDER BY total_betrays DESC;
